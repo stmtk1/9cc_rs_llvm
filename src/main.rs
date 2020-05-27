7bbd72437d237b9f6143a7dfb0f940293a0464fc
斎藤基樹 <stmtk13044032@gmail.com>
2020-05-27 21:58:00 +0900

    add parse error message

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,17 @@
-fn main() -> Result<(), GrammerError> {
+fn main() {
     let args: Vec<String> = std::env::args().collect();
 
     if args.len() != 2 {
         eprintln!("引数が{}個あります。引数は2個にしてください", args.len());
-        return Err(GrammerError::UnexpectedChar);
+        return;
     }
-    let mut parser = Parser::new(&args[1])?;
+    if let Err(err) = parse_main(&args[1]) {
+        eprintln!("{}\n{}",args[1] , err);
+    }
+}
+
+fn parse_main(s: &str) -> Result<(), QccError> {
+    let mut parser = Parser::new(s)?;
 
     println!("define i32 @main() local_unnamed_addr #0 {{");
     println!("entry:");
@@ -21,7 +27,13 @@ struct Lexer <'a> {
 }
 
 #[derive(Clone, Debug)]
-enum Token {
+struct Token {
+    kind: TokenKind,
+    pos: usize,
+}
+
+#[derive(Clone, Debug)]
+enum TokenKind {
     Number(i32),
     Plus,
     Minus,
@@ -36,36 +48,53 @@ impl<'a> Lexer<'a> {
         }
     }
 
-    fn next_token(&mut self) -> Result<Token, GrammerError> {
+    fn next_token(&mut self) -> Result<Token, QccError> {
         while self.pos < self.input.len() && self.input[self.pos] == b' ' {
             self.pos += 1;
         }
         if self.pos >= self.input.len() {
-            return Ok(Token::End);
+            return Ok(Token{
+                kind: TokenKind::End,
+                pos: self.pos,
+            });
         }
         match self.input[self.pos] {
-            b'0'..=b'9' => Ok(Token::Number(self.lex_number())),
+            b'0'..=b'9' => self.lex_number(),
             b'+' => self.lex_plus(),
             b'-' => self.lex_minus(),
-            _ => Err(GrammerError::UnexpectedToken)
+            _ => Err(QccError{
+                kind: ErrorType::UnexpectedChar,
+                pos: self.pos,
+                message: String::from("unknown expression"),
+            })
         }
     }
 
-    fn lex_plus(&mut self) -> Result<Token, GrammerError> {
+    fn lex_plus(&mut self) -> Result<Token, QccError> {
         self.pos += 1;
-        Ok(Token::Plus)
+        Ok(Token{
+            kind: TokenKind::Plus,
+            pos: self.pos - 1,
+        })
     }
 
-    fn lex_minus(&mut self) -> Result<Token, GrammerError> {
+    fn lex_minus(&mut self) -> Result<Token, QccError> {
         self.pos += 1;
-        Ok(Token::Minus)
+        Ok(Token{
+            kind: TokenKind::Minus,
+            pos: self.pos -1,
+        })
     }
 
-    fn lex_number(&mut self) -> i32 {
+    fn lex_number(&mut self) -> Result<Token, QccError> {
         let start = self.pos;
         let end = self.recognize_multiple_chars(|b| b"0123456789".contains(&b));
         self.pos = end;
-        std::str::from_utf8(&self.input[start..end]).unwrap().parse::<i32>().unwrap()
+        let num = std::str::from_utf8(&self.input[start..end]).unwrap().parse::<i32>().unwrap();
+        Ok(Token{
+            kind: TokenKind::Number(num),
+            pos: start,
+        })
     }
 
     fn recognize_multiple_chars(&self, mut f: impl FnMut(u8) -> bool) -> usize {
@@ -85,19 +114,19 @@ struct Parser {
 }
 
 impl Parser {
-    fn new(s: &str) -> Result<Parser, GrammerError> {
+    fn new(s: &str) -> Result<Parser, QccError> {
         let mut lexer = Lexer::new(s);
         let mut tokens = Vec::new();
-        let mut next_token = lexer.next_token();
+        let mut next_token = lexer.next_token()?;
         loop {
-            match &next_token? {
-                Token::End => {
-                    tokens.push(Token::End);
+            match &next_token.kind {
+                TokenKind::End => {
+                    tokens.push(next_token.clone());
                     break;
                 },
-                token => tokens.push(token.clone()),
+                token => tokens.push(next_token.clone()),
             }
-            next_token = lexer.next_token();
+            next_token = lexer.next_token()?;
         }
         Ok(Parser {
             tokens,
@@ -106,47 +135,63 @@ impl Parser {
         })
     }
 
-    fn parse(&mut self) -> Result<(), GrammerError> {
+    fn parse(&mut self) -> Result<(), QccError> {
         let a = self.alloc()?;
-        self.additonal_ops(a)?;
+        self.multiplitive_ops(a)?;
         Ok(())
     }
 
-    fn alloc(&mut self) -> Result<usize, GrammerError> {
-        if let Token::Number(i) = self.tokens[self.pos] {
-            println!("\t%{} = alloca i32, align 4", self.num, );
+    fn alloc(&mut self) -> Result<usize, QccError> {
+        if let TokenKind::Number(i) = self.tokens[self.pos].kind {
+            println!("\t%{} = alloca i32, align 4", self.num);
             println!("\tstore i32 {}, i32* %{}, align 4", i, self.num);
             println!("\t%{} = load i32, i32* %{}, align 4", self.num + 1, self.num);
             self.num += 2;
             self.pos += 1;
             Ok(self.num - 1)
         } else {
-            Err(GrammerError::UnexpectedToken)
+            Err(QccError{
+                kind: ErrorType::UnexpectedToken,
+                pos: self.tokens[self.pos].pos,
+                message: String::from("This expression expected number"),
+            })
         }
     }
 
-    fn additonal_ops(&mut self, initial_pos: usize) -> Result<usize, GrammerError> {
+    fn additonal_ops(&mut self, initial_pos: usize) -> Result<usize, QccError> {
         let mut add_pos = initial_pos;
         loop {
-            match self.tokens[self.pos] {
-                Token::Plus => {
-                    if let Token::Number(i) = self.tokens[self.pos + 1] {
+            match self.tokens[self.pos].kind {
+                TokenKind::Plus => {
+                    if let TokenKind::Number(i) = self.tokens[self.pos + 1].kind {
                         println!("\t%{} = add i32 {}, %{}", self.num, i, add_pos);
                     } else {
-                        return Err(GrammerError::UnexpectedToken);
+                        return Err(QccError {
+                            kind: ErrorType::UnexpectedToken,
+                            pos: self.tokens[self.pos].pos,
+                            message: String::from("This expression expected number"),
+                        });
                     }
                 },
-                Token::Minus => {
-                    if let Token::Number(i) = self.tokens[self.pos + 1] {
+                TokenKind::Minus => {
+                    if let TokenKind::Number(i) = self.tokens[self.pos + 1].kind {
                         println!("\t%{} = sub i32 {}, %{}", self.num, i, add_pos);
                     } else {
-                        return Err(GrammerError::UnexpectedToken);
+                        return Err(QccError{
+                            kind: ErrorType::UnexpectedToken,
+                            pos: self.tokens[self.pos].pos,
+                            message: String::from("This expression expected number"),
+                        });
                     }
                 },
-                Token::End => {
+                TokenKind::End => {
                     break;
                 },
-                _ => return Err(GrammerError::UnexpectedToken),
+                _ => return Err(QccError{
+                    kind: ErrorType::UnexpectedToken,
+                    pos: self.tokens[self.pos].pos,
+                    message: String::from("This expression expected + or -"),
+                }),
             }
             self.pos += 2;
             self.num += 1;
@@ -155,14 +200,31 @@ impl Parser {
         Ok(self.num)
     }
 
-    fn multiplitive_ops(&mut self, initial_pos: usize) -> Result<usize, GrammerError> {
-        Ok(self.num)
+    fn multiplitive_ops(&mut self, initial_pos: usize) -> Result<usize, QccError> {
+        self.additonal_ops(initial_pos)
     }
 }
 
 #[derive(Debug)]
-enum GrammerError {
+enum ErrorType {
     UnexpectedToken,
     UnexpectedChar,
     UnexpectedEOL,
+}
+
+#[derive(Debug)]
+struct QccError {
+    kind: ErrorType,
+    pos: usize,
+    message: String,
+}
+
+
+impl std::fmt::Display for QccError {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        for _ in 0..self.pos {
+            write!(f, " ")?;
+        }
+        write!(f, "^\n{}", self.message)
+    }
 }
